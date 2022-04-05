e59535bfc0982c7509b6156ef2d1c97dda458bc1
KalashKenZo <83351595+KalashKenZo@users.noreply.github.com>
2022-04-05 21:30:46 +0300

    main

---
+++ Animation/CandyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CandyViewController.swift
+//  Animation
+//
+//  Created by Student on 15.02.2022.
+//
+
+import UIKit
+
+class CandyViewController: UIViewController {
+    
+    @IBOutlet weak var candyPicture: UIImageView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: [.repeat,.autoreverse], animations: {
+            self.candyPicture.frame.size.width = 105
+            self.candyPicture.frame.size.height = 54
+        }, completion: nil)
+    }
+}
